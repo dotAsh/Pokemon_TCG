c46b85966d99cf0f88a57b3c0bf5ffc79001a697
dotAsh <ashikshuvo45@gmail.com>
2024-05-20 00:59:05 +0600

    setModalLabelIncluded

--- src/components/SetModal.tsx
+++ src/components/SetModal.tsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import { SetProps } from "../types";
 import useCounter from "@/hooks/react-query-hooks";
@@ -22,18 +21,19 @@ const SetModal: React.FC<SetProps> = ({ set, setSelectedSet }) => {
                 height={300}
                 width={300}
                 alt="set logo"
-                className="h-34"
+                className="my-2 h-34"
               />
-              <h2 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {set.name || "loading...."}
+              <h2 className="my-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Name: {set.name || "loading...."}
               </h2>
-              <h2 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {set.releaseDate || "loading...."}
+              <h2 className="my-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Release Date: {set.releaseDate || "loading...."}
               </h2>
-              <h2 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {set.total || "loading...."}
+              <h2 className="my-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Total: {set.total || "loading...."}
               </h2>
-              <button onClick={() => addToCart(set)} type="button">
+             
+              <button  className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => addToCart(set)} type="button">
                 Add to Cart
               </button>
               <div className="modal-action">
